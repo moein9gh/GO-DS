31c48fa714845396621daeae56437074e77f9d41
moein9gh <moein9gh@gmail.com>
2023-04-18 11:52:40 +0330

    feat: add stack data structure

---
+++ stack.go
@@ -0,0 +1,18 @@
+package main
+
+type Stack struct {
+	items []interface{}
+}
+
+func (s *Stack) Push(item interface{}) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack) Pop() interface{} {
+	if len(s.items) == 0 {
+		return nil
+	}
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return item
+}
